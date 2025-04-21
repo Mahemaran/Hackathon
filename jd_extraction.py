f9ea3009c583e9b5722077227412f261b4ec0a61
Mahemaran <166572866+Mahemaran@users.noreply.github.com>
2025-04-21 09:51:50 +0530

    Update jd_extraction.py

--- jd_extraction.py
+++ jd_extraction.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import sqlite3
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_groq import ChatGroq
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 import numpy as np
+from google import genai
+from google.genai import types
 
 data = pd.read_csv(r"D:\Maran\Project\A Gen AI Sprint\Dataset\job_description.csv", encoding='ISO-8859-1')
 data = data.iloc[:2, :]
@@ -15,43 +15,35 @@ df = data.to_dict(orient='records')
 df_role = [i['Job Title'] for i in df]
 df_jd = [i['Job Description'] for i in df]
 
-sections_to_extract = ["work experience", "Skills"]
-
 work_experience_list, skills_list, work_experience_embeddings_list, skills_embeddings_list = [], [], [], []
 role_embeddings_list, description_embeddings_list = [], []
 
-example_output = """
-Example format:
-Role - Data Scientist
-
-Work Experience - 
-Data Scientist at ABC Inc. (2019-2023)
-Built predictive models that enhanced decision-making processes, reducing operational costs by 25%.
-
-Skills - 
-Cybersecurity - Skilled in penetration testing, risk assessment, and securing enterprise networks against cyber threats.
-"""
-
-api_key = "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"
-llm = ChatGroq(groq_api_key=api_key, model_name="llama3-8b-8192")
 model = SentenceTransformer('all-MiniLM-L6-v2')
+client = genai.Client(api_key="AIzaSyAHdrg4G4Ves-qh8GcNfwh0FiPIGRCsGic")
 
 for role, jd in zip(df_role[:2], df_jd[:2]):
     role_embeddings_list.append(model.encode(role).astype(np.float32).tobytes())
     description_embeddings_list.append(model.encode(jd).astype(np.float32).tobytes())
     
-    for section in sections_to_extract:
-        prompt = f"""
-    Extract only the **{section}** section from the following JD. Use the format shown in the example.
+    prompts = {
+    "experience": f"""
+Extract only the work experience from the following jd text: {jd}. 
+""",
 
-    JD:
-    {jd}
-
-    {example_output}
-    """
-        response = llm.invoke(prompt).content
+    "skills": f"""
+Extract only the skills and tools from the following jd text: {jd}. 
+"""
+}
+    
+    for key, prompt in prompts.items() :
+        responses = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=[prompt],
+        config=types.GenerateContentConfig(
+            temperature=0))
+        response =responses.text.strip()
         
-        if section == "work experience":
+        if key == "experience":
             work_experience_list.append(response)
             work_experience_embeddings_list.append(model.encode(response).astype(np.float32).tobytes())
         else:
@@ -65,6 +57,6 @@ data['work_experience_embeddings'] ,data['skills_embeddings'] = work_experience_
 # Connect to SQLite (or create)
 conn = sqlite3.connect("D:\Maran\Project\A Gen AI Sprint\Dataset\database\smarthire_db.db")
 # Store in a new table called 'jobs'
-data.to_sql("jobs", conn, if_exists="replace", index=False)
-# conn.close()
+data.to_sql("JD", conn, if_exists="replace", index=False)
+conn.close()
 print("✅ Data saved to SQLite!")
